f5d49480fed472bcbececb6510eee7314a0ad988
Fernando Laterza <111194951+fhlaterza@users.noreply.github.com>
2024-12-31 12:32:26 -0300

    ajuste para correção imagens

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: {
       // Configuração para carregar imagens de seu domínio
-    loader: "default", // Use 'default' se não for necessário um carregador customizado
-    domains: [], // Se você usar imagens de domínios externos, adicione-os aqui
-  },
+      unoptimized:true 
+    },
 };
 
 export default nextConfig;
