3e52372caa25027428699f976749437cd485967d
Fernando Laterza <111194951+fhlaterza@users.noreply.github.com>
2024-12-31 12:29:19 -0300

    ajuste para exibição de imagens

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+      // Configuração para carregar imagens de seu domínio
+    loader: "default", // Use 'default' se não for necessário um carregador customizado
+    domains: [], // Se você usar imagens de domínios externos, adicione-os aqui
+  },
 };
 
 export default nextConfig;
